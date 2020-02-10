d39ccad8869ebdac4543c51f2ce3f344fcd4fcde
furie <50117292+sbrodov@users.noreply.github.com>
2020-02-10 12:15:38 +0500

    Delete scroll.js

--- works/newsingapore/js/scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("a.ancLinks").click(function () {
-    var elementClick = $(this).attr("href");
-    var destination = $(elementClick).offset().top;
-    $('html,body').animate( { scrollTop: destination }, 1100 );
-    return false;
-  });
-});
